b0a883255e9a8ba23ccea130a2ab32e877af62c8
ronaldo3030 <ronaldomacielcamposjunior@gmail.com>
2022-04-12 10:10:43 -0300

    add Modulos

---
+++ Modulo_3/Modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraCidade("Ronaldo", "Lagoa Dourada"))
+console.log(mostraIdade("Ronaldo", 22))
+console.log(mostraHobby("Ronaldo", "Estudar"))
